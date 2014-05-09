86b4269adc6973a4894d65fb07af8588704ee7ce
ripxfrostbite <gandvando@hotmail.com>
2014-05-09 04:09:28 -0500

    Working on Space Engineers event thingy, once done it should, if enforced, allow a survival server in Space engineers to manage wars and such to avoid random destruction of builds.

--- src/org/frostbite/karren/space/SpaceController.java
+++ src/org/frostbite/karren/space/SpaceController.java
@@ -1,31 +1,50 @@
 package org.frostbite.karren.space;
 
+import org.frostbite.karren.KarrenBot;
 import org.frostbite.karren.MySQLInterface;
+import org.pircbotx.Channel;
 
 import java.sql.SQLException;
+import java.util.Date;
 
-public class SpaceController {
+public class SpaceController extends Thread {
     private MySQLInterface sql;
     private SpaceFaction[] factions;
     private SpaceEvent[] events;
-    public SpaceController(MySQLInterface sql){
+    private SpaceUser[] users;
+    private boolean stopController;
+    private KarrenBot bot;
+    private Channel announceChannel;
+    public SpaceController(MySQLInterface sql, KarrenBot bot, Channel channel){
         this.sql = sql;
-        loadAllFactions();
-        loadAllEvents();
+        this.announceChannel = channel;
+        loadFromSQL();
     }
-    public void loadAllFactions(){
+    public void loadFromSQL(){
         try {
             factions = sql.loadSpaceFactions();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    public void loadAllEvents(){
-        try {
             events = sql.loadSpaceEvents();
+            users = sql.loadSpaceUsers();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
+    public void killController(){
+        stopController = true;
+    }
+    public void run(){
+        Date date = new Date();
+        while(!stopController){
+            for(SpaceEvent event : events){
+                if(event.getStartDate() <= date.getTime() && !event.hasEventBegun()){
+                    announceChannel.send().message(event.getEventDataFormatted());
+                }
+            }
+            try {
+                sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
