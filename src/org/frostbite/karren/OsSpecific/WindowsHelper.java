3fdf11cb6203ea7a47b6e4a2ba176c45a0aef700
ripxfrostbite <gandvando@hotmail.com>
2014-06-09 03:10:13 -0500

    Fixing Windows not being detected.

--- src/org/frostbite/karren/OsSpecific/WindowsHelper.java
+++ src/org/frostbite/karren/OsSpecific/WindowsHelper.java
@@ -3,7 +3,6 @@ package org.frostbite.karren.OsSpecific;
 import com.sun.jna.LastErrorException;
 import com.sun.jna.Native;
 import com.sun.jna.win32.StdCallLibrary;
-import sun.awt.OSInfo;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,11 +16,8 @@ public class WindowsHelper {
         boolean IsUserAnAdmin() throws LastErrorException;
     }
     public boolean isSystemWindows(){
-        String check = OSInfo.getOSType().name();
-        if(check.contains("Windows"))
-            return true;
-        else
-            return false;
+        String check = System.getProperty("os.name");
+        return check.contains("Windows");
     }
     public boolean checkIfElevated(){
         Shell32 INSTANCE = (Shell32) Native.loadLibrary("shell32", Shell32.class);
