78c946bd92c89cac7e25f4a44aead02626632664
Owen Bennett <gandvando@hotmail.com>
2016-03-14 17:37:18 -0500

    Updated Copyright notices

--- src/org/frostbite/karren/listeners/KillCommand.java
+++ src/org/frostbite/karren/listeners/KillCommand.java
@@ -1,7 +1,11 @@
 /*
- * Copyright 2014 ripxfrostbite
- * Coded by Owen Bennett for the CRaZyPANTS Server Network, released under the MIT Licence, we take no responsibility if something breaks when you change code.
- * If something breaks on the bot and you didn't change anything please log it as an issue so I can fix it.
+ * Copyright (c) 2016 Owen Bennett.
+ *  You may use, distribute and modify this code under the terms of the MIT licence.
+ *  You should have obtained a copy of the MIT licence with this software,
+ *  if not please obtain one from https://opensource.org/licences/MIT
+ *
+ *
+ *
  */
 
 package org.frostbite.karren.listeners;
