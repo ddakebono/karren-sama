8788ac6d131d14fce23e9946734c8adb164fa0bb
Owen Bennett <gandvando@hotmail.com>
2016-04-13 04:29:56 -0500

    Fixed issue with dj info not being set properly after disconnect.

--- src/org/frostbite/karren/Database/MySQLInterface.java
+++ src/org/frostbite/karren/Database/MySQLInterface.java
@@ -67,7 +67,7 @@ public class MySQLInterface {
 
     public void updateDJActivity(String curDJ, String streamName){
         sqlConn.update(RadioDj.RADIO_DJ).set(RadioDj.RADIO_DJ.ACTIVE, 0).execute();
-        if(streamName!=null || curDJ.length()>0) {
+        if(curDJ!=null&&curDJ.length()>0) {
             sqlConn.insertInto(RadioDj.RADIO_DJ).values(null, curDJ, curDJ, streamName, "default", 1).onDuplicateKeyUpdate().set(RadioDj.RADIO_DJ.ACTIVE, 1).set(RadioDj.RADIO_DJ.STREAMNAME, streamName).execute();
         }
     }
