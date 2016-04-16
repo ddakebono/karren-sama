06036f71cf4699f4f00a95144e243ae6dce4cee7
Owen Bennett <gandvando@hotmail.com>
2016-04-16 01:34:28 -0500

    Changed MySQLInterface to ensure the bot maintains a SQL connection

--- src/org/frostbite/karren/Database/MySQLInterface.java
+++ src/org/frostbite/karren/Database/MySQLInterface.java
@@ -33,6 +33,10 @@ public class MySQLInterface {
     private DSLContext sqlConn;
 
     public MySQLInterface(){
+        refreshSQLConnection();
+    }
+
+    private void refreshSQLConnection(){
         try {
             this.sqlConn = DSL.using(DriverManager.getConnection("jdbc:mysql://" + Karren.conf.getSqlhost() + ":" + Karren.conf.getSqlport() + "/" + Karren.conf.getSqldb() + "?useUnicode=true&characterEncoding=UTF-8", Karren.conf.getSqluser(), Karren.conf.getSqlpass()), SQLDialect.MARIADB);
         } catch (SQLException e) {
@@ -41,31 +45,37 @@ public class MySQLInterface {
     }
 
     public WordCountsRecord getWordCount(String word){
+        refreshSQLConnection();
         sqlConn.insertInto(WordCounts.WORD_COUNTS).values(null, word, 1, new Timestamp(new Date().getTime())).onDuplicateKeyIgnore().execute();
         return sqlConn.selectFrom(WordCounts.WORD_COUNTS).where(WordCounts.WORD_COUNTS.WORD.equalIgnoreCase(word)).fetchOne();
     }
 
 
     public UsersRecord getUserData(String nick){
+        refreshSQLConnection();
         sqlConn.insertInto(Users.USERS).values(null, nick, false, 0).onDuplicateKeyIgnore().execute();
         return sqlConn.selectFrom(Users.USERS).where(Users.USERS.USER.equalIgnoreCase(nick)).fetchOne();
     }
 
     public Result<UserfavesRecord> getUserFaves(int songid){
+        refreshSQLConnection();
         return sqlConn.selectFrom(Userfaves.USERFAVES).where(Userfaves.USERFAVES.SONGID.equal(songid)).fetch();
     }
 
     public void addUserFave(String userid, Song song){
+        refreshSQLConnection();
         sqlConn.insertInto(Userfaves.USERFAVES).values(null, userid, song.getSongID()).onDuplicateKeyIgnore().execute();
         song.addFave();
     }
 
     public SongdbRecord getSong(String name){
+        refreshSQLConnection();
         sqlConn.insertInto(Songdb.SONGDB).values(null, name, 0, 0, 0, 0, false).onDuplicateKeyIgnore().execute();
         return sqlConn.selectFrom(Songdb.SONGDB).where(Songdb.SONGDB.SONGTITLE.equal(name)).fetchOne();
     }
 
     public void updateDJActivity(String curDJ, String streamName){
+        refreshSQLConnection();
         sqlConn.update(RadioDj.RADIO_DJ).set(RadioDj.RADIO_DJ.ACTIVE, 0).execute();
         if(curDJ!=null&&curDJ.length()>0) {
             sqlConn.insertInto(RadioDj.RADIO_DJ).values(null, curDJ, curDJ, streamName, "default", 1).onDuplicateKeyUpdate().set(RadioDj.RADIO_DJ.ACTIVE, 1).set(RadioDj.RADIO_DJ.STREAMNAME, streamName).execute();
