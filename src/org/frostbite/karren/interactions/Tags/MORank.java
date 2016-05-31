76ae89bbbe337b7a5b38522d0fd48c4718731699
Owen Bennett <gandvando@hotmail.com>
2016-05-31 16:23:16 -0500

    Roughly finished off the overwatch ranking stuff

--- src/org/frostbite/karren/interactions/Tags/MORank.java
+++ src/org/frostbite/karren/interactions/Tags/MORank.java
@@ -16,6 +16,7 @@ import org.apache.commons.lang3.StringEscapeUtils;
 import org.frostbite.karren.interactions.Interaction;
 import org.frostbite.karren.interactions.Tag;
 import org.frostbite.karren.interactions.Tags.TagObjects.HeroSearchResults;
+import org.frostbite.karren.interactions.Tags.TagObjects.MasterOverwatchHeroes;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
@@ -29,14 +30,67 @@ public class MORank implements Tag {
     public String handleTemplate(String msg, Interaction interaction, MessageBuilder response, MessageReceivedEvent event) {
         String parameter = interaction.getParameter();
         interaction.setParameter("");
+        String[] args = parameter.split(",");
         Gson gson = new Gson();
         try {
-            HeroSearchResults results = gson.fromJson(IOUtils.toString(new URL("https://masteroverwatch.com/leaderboards/pc/us/hero/20/score/search?name=DDAkebono").openStream()), HeroSearchResults.class);
+            HeroSearchResults results = gson.fromJson(IOUtils.toString(new URL("https://masteroverwatch.com/leaderboards/pc/us/hero/ " + getHeroNumber(args[1]).getHeroNumber() + "/score/search?name=" + args[0].trim()).openStream()), HeroSearchResults.class);
             Document result = Jsoup.parse(StringEscapeUtils.unescapeJava(results.getSingleEntry()));
             msg = msg.replace("%rank", result.getElementsByClass("table-icon-rank").get(0).text());
+            msg = msg.replace("%hero", args[1].trim());
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (NullPointerException e){
+            msg = interaction.getRandomTemplatesFail();
+        } catch (ArrayIndexOutOfBoundsException e){
+            msg = interaction.getRandomTemplatesPermError();
         }
         return msg;
     }
+
+    private MasterOverwatchHeroes getHeroNumber(String hero){
+        switch(hero.trim().toLowerCase()){
+            case "bastion":
+                return MasterOverwatchHeroes.BASTION;
+            case "genji":
+                return MasterOverwatchHeroes.GENJI;
+            case "hanzo":
+                return MasterOverwatchHeroes.HANZO;
+            case "junkrat":
+                return MasterOverwatchHeroes.JUNKRAT;
+            case "lucio":
+                return MasterOverwatchHeroes.LUCIO;
+            case "mccree":
+                return MasterOverwatchHeroes.MCCREE;
+            case "mei":
+                return MasterOverwatchHeroes.MEI;
+            case "mercy":
+                return MasterOverwatchHeroes.MERCY;
+            case "pharah":
+                return MasterOverwatchHeroes.PHARAH;
+            case "reaper":
+                return MasterOverwatchHeroes.REAPER;
+            case "reinhardt":
+                return MasterOverwatchHeroes.REINHARDT;
+            case "roadhog":
+                return MasterOverwatchHeroes.ROADHOG;
+            case "soldier76":
+                return MasterOverwatchHeroes.SOLDIER76;
+            case "symmetra":
+                return MasterOverwatchHeroes.SYMMETRA;
+            case "torbjorn":
+                return MasterOverwatchHeroes.TORBJORN;
+            case "tracer":
+                return MasterOverwatchHeroes.TRACER;
+            case "widowmaker":
+                return MasterOverwatchHeroes.WIDOWMAKER;
+            case "winston":
+                return MasterOverwatchHeroes.WINSTON;
+            case "zarya":
+                return MasterOverwatchHeroes.ZARYA;
+            case "zenyatta":
+                return MasterOverwatchHeroes.ZENYATTA;
+        }
+        return null;
+    }
+
 }
