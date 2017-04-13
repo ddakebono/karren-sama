939706715c584c416a1c0ce173103692094abc18
Owen Bennett <gandvando@hotmail.com>
2017-04-13 01:31:28 -0500

    Fixed maven!

--- src/org/frostbite/karren/Karren.java
+++ src/org/frostbite/karren/Karren.java
@@ -10,17 +10,13 @@
 
 package org.frostbite.karren;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sx.blah.discord.api.ClientBuilder;
-import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.util.DiscordException;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class Karren{
 
