9c87ecccc5f1e29258e84600ec95d71fff72da01
Owen Bennett <gandvando@hotmail.com>
2017-06-25 22:11:37 -0500

    Fixed an issue with RoleRoll crashing on servers with no lotto- roles.

--- src/org/frostbite/karren/interactions/Tags/RoleRoll.java
+++ src/org/frostbite/karren/interactions/Tags/RoleRoll.java
@@ -30,43 +30,47 @@ public class RoleRoll implements Tag {
     public String handleTemplate(String msg, Interaction interaction, MessageBuilder response, MessageReceivedEvent event) {
         if(!event.getMessage().getChannel().isPrivate()) {
             DbGuildUser dbGuildUser = Karren.bot.getSql().getGuildUser(event.getGuild(), event.getAuthor());
-            if (dbGuildUser.getRollTimeout() == null || new Timestamp(System.currentTimeMillis()).after(dbGuildUser.getRollTimeout())) {
-                dbGuildUser.incrementTotalRolls();
-                java.util.Random rng = new Random();
-                int roll = rng.nextInt(100);
-                int bonus = (dbGuildUser.getRollsSinceLastClear() / 2);
-                int dc = (Karren.bot.getSql().getGuild(event.getGuild()).getRollDifficulty() >= 0 ? Karren.bot.getSql().getGuild(event.getGuild()).getRollDifficulty() : 95);
-                Karren.log.info("Rolled " + roll + " against a DC of " + dc + " with bonus of " + bonus);
-                roll+=bonus;
-                if (roll >= dc) {
-                    List<IRole> rollRoles = new LinkedList<>();
-                    for (IRole role : event.getGuild().getRoles())
-                        if (role.getName().contains("lotto-"))
-                            rollRoles.add(role);
-                    IRole rngRole = rollRoles.get(rng.nextInt(rollRoles.size()));
-                    for(IRole role : event.getAuthor().getRolesForGuild(event.getGuild()))
-                        if(role.getName().contains("lotto-"))
-                            event.getAuthor().removeRole(role);
-                    event.getAuthor().addRole(rngRole);
-                    msg = msg.replace("%rngrole", rngRole.getName());
-                    dbGuildUser.setRollsSinceLastClear(0);
-                    dbGuildUser.setRollTimeout(new Timestamp(System.currentTimeMillis() + 259200000 ));
+            List<IRole> rollRoles = new LinkedList<>();
+            for (IRole role : event.getGuild().getRoles())
+                if (role.getName().contains("lotto-"))
+                    rollRoles.add(role);
+            if(rollRoles.size()>0) {
+                if (dbGuildUser.getRollTimeout() == null || new Timestamp(System.currentTimeMillis()).after(dbGuildUser.getRollTimeout())) {
+                    dbGuildUser.incrementTotalRolls();
+                    java.util.Random rng = new Random();
+                    int roll = rng.nextInt(100);
+                    int bonus = (dbGuildUser.getRollsSinceLastClear() / 2);
+                    int dc = (Karren.bot.getSql().getGuild(event.getGuild()).getRollDifficulty() >= 0 ? Karren.bot.getSql().getGuild(event.getGuild()).getRollDifficulty() : 95);
+                    Karren.log.info("Rolled " + roll + " against a DC of " + dc + " with bonus of " + bonus);
+                    roll += bonus;
+                    if (roll >= dc) {
+                        IRole rngRole = rollRoles.get(rng.nextInt(rollRoles.size()));
+                        for (IRole role : event.getAuthor().getRolesForGuild(event.getGuild()))
+                            if (role.getName().contains("lotto-"))
+                                event.getAuthor().removeRole(role);
+                        event.getAuthor().addRole(rngRole);
+                        msg = msg.replace("%rngrole", rngRole.getName());
+                        dbGuildUser.setRollsSinceLastClear(0);
+                        dbGuildUser.setRollTimeout(new Timestamp(System.currentTimeMillis() + 259200000));
+                    } else {
+                        dbGuildUser.incrementRollsSinceLastClear();
+                        dbGuildUser.setRollTimeout(new Timestamp(System.currentTimeMillis() + 21600000));
+                        msg = interaction.getRandomTemplate("fail").getTemplate();
+                    }
+                    msg = msg.replace("%bonus", String.valueOf(bonus));
+                    msg = msg.replace("%roll", String.valueOf(roll));
+                    msg = msg.replace("%dc", String.valueOf(dc));
+                    dbGuildUser.update();
                 } else {
-                    dbGuildUser.incrementRollsSinceLastClear();
-                    dbGuildUser.setRollTimeout(new Timestamp(System.currentTimeMillis() + 21600000));
-                    msg = interaction.getRandomTemplate("fail").getTemplate();
-                }
-                msg = msg.replace("%bonus", String.valueOf(bonus));
-                msg = msg.replace("%roll", String.valueOf(roll));
-                msg = msg.replace("%dc", String.valueOf(dc));
-                dbGuildUser.update();
-            } else {
-                msg = interaction.getRandomTemplate("permission").getTemplate();
+                    msg = interaction.getRandomTemplate("permission").getTemplate();
 
+                }
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM @ HH:mm");
+                msg = msg.replace("%timeremaining", dateFormat.format(dbGuildUser.getRollTimeout()));
+                return msg;
+            } else {
+                return interaction.getRandomTemplate("noroles").getTemplate();
             }
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM @ HH:mm");
-            msg = msg.replace("%timeremaining", dateFormat.format(dbGuildUser.getRollTimeout()));
-            return msg;
         }
         return "This cannot be used in a private message!";
     }
