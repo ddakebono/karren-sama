03b721b4ba21c533fb8162418c514e2128a570f7
DDAkebono <gandvando@hotmail.com>
2020-10-26 23:37:20 -0500

    Updated Lavaplayer to fix Youtube API issues

--- src/org/frostbite/karren/AudioPlayer/AudioProvider.java
+++ src/org/frostbite/karren/AudioPlayer/AudioProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Owen Bennett.
+ * Copyright (c) 2020 Owen Bennett.
  *  You may use, distribute and modify this code under the terms of the MIT licence.
  *  You should have obtained a copy of the MIT licence with this software,
  *  if not please obtain one from https://opensource.org/licences/MIT
@@ -9,7 +9,6 @@
  */
 
 package org.frostbite.karren.AudioPlayer;
-
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
 import net.dv8tion.jda.api.audio.AudioSendHandler;
