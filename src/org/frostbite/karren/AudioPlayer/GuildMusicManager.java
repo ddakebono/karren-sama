cedbba8e2c947f440b0f05f0d37eb0f9dd6fe62a
DDAkebono <gandvando@hotmail.com>
2021-04-03 01:05:21 -0500

    Updated Dependencies

--- src/org/frostbite/karren/AudioPlayer/GuildMusicManager.java
+++ src/org/frostbite/karren/AudioPlayer/GuildMusicManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Owen Bennett.
+ * Copyright (c) 2021 Owen Bennett.
  *  You may use, distribute and modify this code under the terms of the MIT licence.
  *  You should have obtained a copy of the MIT licence with this software,
  *  if not please obtain one from https://opensource.org/licences/MIT
@@ -51,7 +51,7 @@ public class GuildMusicManager {
       if (event.getMember().getVoiceState() != null) {
         if (event.getMember().getVoiceState().inVoiceChannel()) {
           AudioManager am = event.getGuild().getAudioManager();
-          if (!am.isConnected() && !am.isAttemptingToConnect()) {
+          if (!am.isConnected()) {
             //Connect to chat
             am.openAudioConnection(event.getMember().getVoiceState().getChannel());
           }
